699c9e3d3d751812d65aa18ad8468cff563dc722
tobias ns <13521090@std.stei.itb.ac.id>
2024-05-22 00:37:01 +0700

    fix: stopper movement

---
+++ Assets/Scripts/BranchScript.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BranchScript : MonoBehaviour
+{
+    public bool R;
+    public bool L;
+    public bool T;
+    public bool B;
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -17,27 +17,31 @@ public class PlayerMovement : MonoBehaviour
     public bool isStoping = true;
     public float stopingConstraint = 1.2f;
     private Vector3 targetPosition;
+    [SerializeField] private BranchScript branch;
     [SerializeField] private Animator animator;
 
 
     private void Awake()
     {
         playerRigidbody = GetComponent<Rigidbody>();
+        branch = GetComponent<BranchScript>();
         currentSpeed = defaultSpeed;
         targetPosition = transform.position;
     }
 
     private void Update()
     {
-        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
-        {
-            startPosition = Input.GetTouch(0).position;
-        }
+        if (!hasInput){
+            if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+            {
+                startPosition = Input.GetTouch(0).position;
+            }
 
-        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Ended)
-        {
-            endPosition = Input.GetTouch(0).position;
-            hasInput = true;
+            if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Ended)
+            {
+                endPosition = Input.GetTouch(0).position;
+                hasInput = true;
+            }
         }
         if (isStoping)
         {
@@ -52,25 +56,25 @@ public class PlayerMovement : MonoBehaviour
     {
         if (hasInput)
         {
-            if (endPosition.y > startPosition.y && endPosition.x > startPosition.x)
+            if (endPosition.y > startPosition.y && endPosition.x > startPosition.x && branch.R)
             {
                 animator.SetBool("isMoving", true);
                 animator.SetInteger("rLTB", 2);
                 playerRigidbody.velocity = new Vector3(currentSpeed, 0, 0);
             }
-            else if (endPosition.y > startPosition.y && endPosition.x < startPosition.x)
+            else if (endPosition.y > startPosition.y && endPosition.x < startPosition.x && branch.T)
             {
                 animator.SetBool("isMoving", true);
                 animator.SetInteger("rLTB", 3);
                 playerRigidbody.velocity = new Vector3(0, 0, currentSpeed);
             }
-            else if (endPosition.y < startPosition.y && endPosition.x > startPosition.x)
+            else if (endPosition.y < startPosition.y && endPosition.x > startPosition.x && branch.B)
             {
                 animator.SetBool("isMoving", true);
                 animator.SetInteger("rLTB", 0);
                 playerRigidbody.velocity = new Vector3(0, 0, -currentSpeed);
             }
-            else if (endPosition.y < startPosition.y && endPosition.x < startPosition.x)
+            else if (endPosition.y < startPosition.y && endPosition.x < startPosition.x && branch.L)
             {
                 playerRigidbody.velocity = new Vector3(-currentSpeed, 0, 0);
                 animator.SetBool("isMoving", true);
@@ -99,6 +103,7 @@ public class PlayerMovement : MonoBehaviour
     {
         if (col.CompareTag("Branch"))
         {
+            branch = col.gameObject.GetComponent<BranchScript>();
             targetPosition = col.gameObject.transform.position;
             isStoping = true;
         }
