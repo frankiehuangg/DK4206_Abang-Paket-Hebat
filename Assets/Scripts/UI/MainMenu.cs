77158f831c92f42756b6bed259176fc07bab8fe4
Chris Albert <christiangultom5@gmail.com>
2024-06-02 20:05:03 +0700

    fix: main menu script

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -1,10 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public TextMeshProUGUI coinsText;
+    public TextMeshProUGUI skillZoomOutText;
+    public TextMeshProUGUI skillSpeedUpText;
+    private void Awake()
+    {
+        coinsText.text = GameState.instance.coins.ToString();
+        skillZoomOutText.text = GameState.instance.zoomOut.ToString();
+        skillSpeedUpText.text = GameState.instance.speedUp.ToString();
+    }
     public void StartGame(string mainScene)
     {
         SceneManager.LoadScene(mainScene);
