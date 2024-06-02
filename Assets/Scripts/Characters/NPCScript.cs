299ea78a105ac0b3716a928844a33ef0ee1a8ff7
tobias ns <13521090@std.stei.itb.ac.id>
2024-06-02 22:40:11 +0700

    fit

--- Assets/Scripts/Characters/NPCScript.cs
+++ Assets/Scripts/Characters/NPCScript.cs
@@ -9,6 +9,13 @@ public class NPCScript : MonoBehaviour
     [SerializeField] private Animator animator;
     public Sprite[] npcSprites;
     private SpriteRenderer spriteRenderer;
+    public int waitingDuration = 10;
+    public int waitingCountdown = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
     private void Awake()
     {
         spriteRenderer = GetComponentInChildren<SpriteRenderer>();
@@ -16,6 +23,18 @@ public class NPCScript : MonoBehaviour
         RandomizeSprite();
     }
 
+    void Update()
+    {
+        if(waitingCountdown <= 0) StartCoroutine(StartWaiting());
+
+        if(waitingCountdown < (float)waitingDuration / 3) {
+            SetMad();
+        } else if(waitingCountdown < (float)waitingDuration * 2 / 3) {
+            SetPoker();
+        } else {
+            SetHappy();
+        }
+    }
     private void RandomizeSprite()
     {
         if (spriteRenderer != null)
@@ -25,7 +44,10 @@ public class NPCScript : MonoBehaviour
         }
     }
 
-    public void setHappy() {
+
+    // Update is called once per frame
+
+    public void SetHappy() {
         animator.SetInteger("mood", 0);
     }
 
