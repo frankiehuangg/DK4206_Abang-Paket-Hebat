f6ed5fdcedabf8c173a6030c946c5118d2561ac4
tobias ns <13521090@std.stei.itb.ac.id>
2024-06-02 22:33:14 +0700

    fit

--- Assets/Scripts/Characters/NPCScript.cs
+++ Assets/Scripts/Characters/NPCScript.cs
@@ -6,27 +6,54 @@ using UnityEngine;
 public class NPCScript : MonoBehaviour
 {
     [SerializeField] private Animator animator;
+    [SerializeField] private SpriteRenderer sprite;
+    public Sprite[] sprites;
+    public int waitingDuration = 10;
+    public int waitingCountdown = 0;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(waitingCountdown <= 0) StartCoroutine(StartWaiting());
+
+        if(waitingCountdown < (float)waitingDuration / 3) {
+            SetMad();
+        } else if(waitingCountdown < (float)waitingDuration * 2 / 3) {
+            SetPoker();
+            Debug.Log("a");
+        } else {
+            SetHappy();
+        }
     }
 
-    public void setHappy() {
+    public void SetSprite(int id) {
+        sprite.sprite = sprites[id];
+    }
+
+    public void SetHappy() {
         animator.SetInteger("mood", 0);
     }
 
-    public void setPoker() {
+    public void SetPoker() {
         animator.SetInteger("mood", 1);
     }
 
-    public void setMad() {
+    public void SetMad() {
         animator.SetInteger("mood", 2);
+    }
+
+
+
+    IEnumerator StartWaiting () {
+        waitingCountdown = waitingDuration;
+        while (waitingCountdown > 0) {
+            yield return new WaitForSeconds (1);
+            waitingCountdown--;
+        }
     }
 }
