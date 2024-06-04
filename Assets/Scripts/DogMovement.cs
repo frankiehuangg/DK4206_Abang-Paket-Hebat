1736b8a221dcce36f7fb9568e5cb2150053700d6
frankiehuangg <13521092@std.stei.itb.ac.id>
2024-06-04 18:23:19 +0700

    feat: chasing dog obstacle

---
+++ Assets/Scripts/DogMovement.cs
@@ -0,0 +1,63 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class DogMovement : MonoBehaviour
+{
+    public Transform playerTransform;
+
+    public DeliveryManager deliveryManager;
+
+    private Vector3 spawnPoint;
+
+    private NavMeshAgent agent;
+
+    private float playerDistance;
+
+    private float minDistance = 0.5f;
+
+    private bool finishedChasing = false;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        spawnPoint = transform.position;
+    }
+
+    void Update()
+    {
+        if (finishedChasing)
+        {
+            if (Vector3.Distance(transform.position, spawnPoint) < minDistance)
+            {
+                finishedChasing = false;
+            }
+            else
+            {
+                agent.destination = spawnPoint;
+            }
+        }
+
+        if (!finishedChasing)
+        {
+            playerDistance = Vector3.Distance(transform.position, playerTransform.position);
+
+            if (playerDistance < 20f)
+            {
+                agent.destination = playerTransform.position;
+            }
+            else
+            {
+                agent.destination = spawnPoint;
+            }
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            finishedChasing = true;
+            deliveryManager.isDelivering = false;
+        }
+    }
+}
